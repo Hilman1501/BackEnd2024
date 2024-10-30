34c6ba7b2e61cc4d7c7876c42b3cc3a7d7cdf082
Hilman <146170591+Hilman1501@users.noreply.github.com>
2024-10-30 13:31:22 +0700

    AnimalController.php

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -1,10 +1,41 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\Animal;
 use Illuminate\Http\Request;
 
 class AnimalController extends Controller
 {
-    //
+    // menampilkan semua data hewan
+    public function index()
+    {
+        return Animal::all();
+    }
+
+    // Menyimpan data hewan baru
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            
+        ]);
+
+        $animal = Animal::create($request->all());
+        return response()->json($animal, 201);
+    }
+
+    // Mengupdate data hewan 
+    public function update(Request $request, $id)
+    {
+        $animal = Animal::findOrFail($id);
+        $animal->update($request->all());
+        return response()->json($animal, 200);
+    }
+
+    // Menghapus data hewan
+    public function destroy($id)
+    {
+        Animal::destroy($id);
+        return response()->json(null, 204);
+    }
 }
