1f6d2a7c935d475de177928f19ccc3e935abcf89
Hilman <146170591+Hilman1501@users.noreply.github.com>
2024-10-30 13:33:06 +0700

    api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimalController;
+
+Route::get('animals', [AnimalController::class, 'index'])->name('animals.index');
+Route::post('animals', [AnimalController::class, 'store'])->name('animals.store');
+Route::put('animals/{id}', [AnimalController::class, 'update'])->name('animals.update');
+Route::delete('animals/{id}', [AnimalController::class, 'destroy'])->name('animals.destroy');
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
