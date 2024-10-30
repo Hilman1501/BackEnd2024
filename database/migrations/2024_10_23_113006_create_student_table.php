744a70c488531ec22ea48fcaa8dfb7e075994fc2
Hilman <146170591+Hilman1501@users.noreply.github.com>
2024-10-30 14:43:47 +0700

    2024_10_23_113006_create_student_table.php

--- database/migrations/2024_10_23_113006_create_student_table.php
+++ database/migrations/2024_10_23_113006_create_student_table.php
@@ -1,31 +1,24 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateStudentsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('student', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('nim');
-            $table->string('email');
+            $table->string('nim')->unique();
+            $table->string('email')->unique();
             $table->string('jurusan');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('student');
+        Schema::dropIfExists('students');
     }
-};
+}
