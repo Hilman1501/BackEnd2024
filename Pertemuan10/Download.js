6e3fa2da950b73ba8e5c7c024b2e256d2c6e49b2
Hilman <146170591+Hilman1501@users.noreply.github.com>
2024-12-18 20:53:19 +0700

    Download.js

--- Pertemuan10/Download.js
+++ Pertemuan10/Download.js
@@ -8,7 +8,7 @@ const showDownload = (result) => {
 const download = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const status = true; 
+      const status = true;
       if (status) {
         const result = 'windows-10.exe';
         resolve(result);
@@ -19,14 +19,11 @@ const download = () => {
   });
 };
 
-// Memanggil fungsi download dan menangani hasilnya dengan Async/Await
-const startDownload = async () => {
-  try {
-    const result = await download();
-    showDownload(result);
-  } catch (error) {
-    console.error(error);
-  }
-};
 
-startDownload();
+download()
+  .then((result) => {
+    showDownload(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
